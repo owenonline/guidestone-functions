0308c54ab4bba8d5390947d7cf4b29b1f9076769
Owen Burns <owenburns88@gmail.com>
2024-02-16 21:40:51 -0800

    set up db connections

--- function_app.py
+++ function_app.py
@@ -1,25 +1,29 @@
 import azure.functions as func
+from gremlin_python.driver import client, serializer
+import os
 import logging
+from psycopg2 import pool
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="healthcheck")
+@app.function_name("healthcheck")
+@app.route(route="healthcheck",
+           auth_level=func.AuthLevel.ANONYMOUS, 
+           methods=['GET','POST'])
 def healthcheck(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # verify gremlin connection
+    graph_client = client.Client('wss://docs-graph.gremlin.cosmosdb.azure.com:443/','g', 
+                    username=f"/dbs/guidestone/colls/knowledge-graph", 
+                    password=os.getenv("DOCS_GRAPH_KEY"),
+                    message_serializer=serializer.GraphSONSerializersV2d0())
+    
+    # verify postgres connection
+    postgreSQL_pool = pool.SimpleConnectionPool(1, int(os.getenv("PYTHON_THREADPOOL_THREAD_COUNT")), os.getenv("POSTGRES_CONN_STRING"))  
+    conn = postgreSQL_pool.getconn()
+    cursor = conn.cursor()
+    postgreSQL_pool.putconn(conn)
+    postgreSQL_pool.closeall()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse(
+        status_code=200
+    )
